8fc9a864faca1535bb4af19752ef0d28a4aa2b42
Andrei Lucaci <lucaciandrei@outlook.com>
2020-05-20 20:05:30 +0300

    updated ransomNote

--- interview-preparation-kit/dictionariesAndHashmaps/ransomNote.js
+++ interview-preparation-kit/dictionariesAndHashmaps/ransomNote.js
@@ -1,11 +1,11 @@
 function checkMagazine(magazine, notes) {
   const magazineObj = magazine.reduce((acc, cur) => {
-    (acc[cur] || (acc[cur] = [])).push(cur);
+    acc[cur] = (acc[cur] || 0) + 1;
     return acc;
   }, {});
 
   for (let note of notes) {
-    if (!magazineObj[note] || !magazineObj[note].pop()) {
+    if (!magazineObj[note] || !magazineObj[note]--) {
       console.log("No");
       return;
     }
